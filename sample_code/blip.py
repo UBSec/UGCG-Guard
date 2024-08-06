d3ab48c122a41e10fcfec44e0cc12b77dafd933d
Keyan <90410389+keyanUB@users.noreply.github.com>
2024-08-06 15:48:20 -0400

    Update blip.py

--- sample_code/blip.py
+++ sample_code/blip.py
@@ -9,15 +9,12 @@ import time
 
 
 def get_blip_model(device='cuda', dtype=torch.bfloat16, use_multi_gpus=True):
-  model = InstructBlipForConditionalGeneration.from_pretrained("Salesforce/instructblip-vicuna-13b", offload_folder = 'off', cache_dir="/work/iuy704/hfcache")
+  model = InstructBlipForConditionalGeneration.from_pretrained("Salesforce/instructblip-vicuna-13b", offload_folder = 'off')
   processor = InstructBlipProcessor.from_pretrained("Salesforce/instructblip-vicuna-13b")
   #model.to(dtype)
+  
   if use_multi_gpus:
-    #device_map = infer_auto_device_map(model, max_memory={0: "16GiB", 1: "16GiB", 2: "16GiB", 3: "16GiB"}, no_split_module_classes=['LlamaDecoderLayer', 'VisionTransformer'])
-    #device_map = infer_auto_device_map(model, max_memory={0: "16GiB", 1: "16GiB", 2: "16GiB", 3: "16GiB"},no_split_module_classes=['LlamaForCausalLM', 'InstructBlipVisionModel', 'InstructBlipQFormerModel'])
     device_map = infer_auto_device_map(model, max_memory={0: "28GiB", 1: "28GiB"},no_split_module_classes=['InstructBlipVisionModel', 'InstructBlipQFormerModel', 'LlamaDecoderLayer'])
-    # device_map = infer_auto_device_map(model, max_memory={0: "24GiB", 'cpu': "80GiB"},no_split_module_classes=['InstructBlipVisionModel', 'InstructBlipQFormerModel', 'LlamaDecoderLayer'])
-    #device_map['llm_model.lm_head'] = device_map['llm_proj'] = device_map['llm_model.model.embed_tokens']
     device_map['language_model.lm_head'] = device_map['language_projection'] = device_map[('language_model.model.embed_tokens')]
     print(device_map)
     model = dispatch_model(model, device_map=device_map, offload_dir = 'off')
@@ -39,13 +36,12 @@ df = pd.read_csv("ugcg.csv")
 prompt = """
 This is an image generated from a role-playing game. View the given image and carefully answer the questions in each step:
 Q1: Are there any characters or avatars in the image?
-Q2A: Are the characters or avatars naked?
-Q2B: Are there anuses, female breasts, female genitals, or male genitals exposed in this image?
-Q2C: Are there characters or avatars indulging in any sexual activities?
-Q3A: Are there any knives, guns, or deadly weapons presented in this image?
-Q3B: Are the characters or avatars indulging in any violent activities, such as murder, suicide, and torture?
-Q4: Are the characters or avatars indulging in any other harmful activities, such as smoking, self-harming, or using drugs?
-Q5: If any of your answers for Q2A, Q2B, Q2C, Q3A, Q3B, Q4 is yes, please label the image as "unsafe". Otherwise, please label the image as "safe".
+Q2: Are the characters or avatars naked?
+Q3: Are there anuses, female breasts, female genitals, or male genitals exposed in this image?
+Q4: Are there characters or avatars indulging in any sexual activities?
+Q5: Are there any knives, guns, or deadly weapons presented in this image?
+Q6: Are the characters or avatars indulging in any violent activities, such as murder, suicide, and torture?
+Q7: Are the characters or avatars indulging in any other harmful activities, such as smoking, self-harming, or using drugs?
 """
 
 def blip_output(image_path, prompt):
@@ -55,7 +51,7 @@ def blip_output(image_path, prompt):
   outputs = model.generate(
     **inputs,
     do_sample=True,
-    max_length=1000,
+    max_length=10000,
     min_length=1,
     top_p=0.9,
     #repetition_penalty=1.5,
@@ -70,4 +66,4 @@ df['blip_output'] = df.apply(lambda x: blip_output(x['img_path'], prompt), axis=
 
 
 # save the output
-df.to_csv("ugcg_output.csv", index=False)
+df.to_csv("ugcg_blip.csv", index=False)
